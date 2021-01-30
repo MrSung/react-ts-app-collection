70cf521442547e62b9701ff9cd3709354e3a968c
Sungjoon Park <sungjoon512@gmail.com>
2021-01-30 19:39:04 +0900

    remove unnecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>App</div>
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+  document.getElementById('root')
+)
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
-  import.meta.hot.accept();
+  import.meta.hot.accept()
 }
